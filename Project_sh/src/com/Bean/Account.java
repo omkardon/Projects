70ac455ffaafeeb057bae2236f8ff6a4e142519d
omkardon <omkardon@users.noreply.github.com>
2015-11-07 22:39:48 +0530

    Create Account.java

---
+++ Project_sh/src/com/Bean/Account.java
@@ -0,0 +1,54 @@
+package com.Bean;
+
+public class Account {
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private int number;
+	private String altEmail;
+	private String country;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getAltEmail() {
+		return altEmail;
+	}
+	public void setAltEmail(String altEmail) {
+		this.altEmail = altEmail;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}
